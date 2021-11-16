0b7ef953f4714a3b593276bc1e6374f46174d673
Scharkey <jordanscharkey@gmail.com>
2021-11-16 10:12:35 -0800

    Trackless rotation working, needs adjustments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,12 +16,23 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        /* Movement Information */
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         dir.x = horizontal * speed;
         dir.z = vertical * speed;
         controller.Move(dir * Time.deltaTime);
+        
+        /*
+         * Rotation
+         * Found this nifty set of equations on the following forum post:
+         * https://forum.unity.com/threads/rotating-an-object-to-face-the-mouse-location.21342/
+         */
+        float h = Input.mousePosition.x - Screen.width / 2;
+        float v = Input.mousePosition.y - Screen.height / 2;
+        float angle = -Mathf.Atan2(v,h) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler (0, angle, 0);
     }
 }
