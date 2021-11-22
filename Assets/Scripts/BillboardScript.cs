b2329cb8269703c0651712ba92d8fbe12142ff20
Damon Bowlin <d_bowlin@u.pacific.edu>
2021-11-21 20:25:16 -0800

    Began working on implementing enemy health bars

--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBarScript : MonoBehaviour
+public class BillboardScript : MonoBehaviour
 {
-    
+    private GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation = Camera.main.gameObject.transform.rotation;
+        //gameObject.transform.rotation = Camera.main.gameObject.transform.rotation;
+        gameObject.transform.LookAt(camera.gameObject.transform);
     }
 }
